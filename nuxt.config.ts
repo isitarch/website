0fbdaf4506758a75f937bf8e4160d7efc3858569
Roland Steinegger <roland@steinegger.one>
2024-12-05 23:52:26 +0100

    Avoids missing locale in url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,7 +32,7 @@ export default defineNuxtConfig({
     }
     ],
     langDir: '../locales/',
-    strategy: 'prefix_and_default',
+    strategy: 'prefix',
     detectBrowserLanguage: {
       fallbackLocale: 'en'
     }
